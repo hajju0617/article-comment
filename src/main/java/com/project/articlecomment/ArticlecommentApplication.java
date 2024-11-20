a8a127e4084e8a80139fd6249f6029e3bf6ee225
hajju0617 <joowon023617@gmail.com>
2024-11-20 19:23:41 +0900

    commit

---
+++ src/main/java/com/project/articlecomment/ArticlecommentApplication.java
@@ -0,0 +1,13 @@
+package com.project.articlecomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlecommentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlecommentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/articlecomment/ArticlecommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.articlecomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlecommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
