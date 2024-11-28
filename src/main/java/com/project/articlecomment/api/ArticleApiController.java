393e2ae8f4ec9b3e1609ca4ad14521c998d4fe1d
hajju0617 <joowon023617@gmail.com>
2024-11-28 17:46:37 +0900

    공부공부 복습복습

--- src/main/java/com/project/articlecomment/api/ArticleApiController.java
+++ src/main/java/com/project/articlecomment/api/ArticleApiController.java
@@ -6,6 +6,8 @@ import com.project.articlecomment.entity.Article;
 import com.project.articlecomment.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,4 +33,29 @@ public class ArticleApiController {
         Article article = dto.toEntity();
         return articleRepository.save(article);
     }
+
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        log.info("id = {}, article = {}", id, article.toString());
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null || id != article.getId()) {
+            log.info("잘못된 요청임 id = {}, article = {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
--- src/main/java/com/project/articlecomment/dto/ArticleForm.java
+++ src/main/java/com/project/articlecomment/dto/ArticleForm.java
@@ -10,14 +10,16 @@ import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
+@Getter
 public class ArticleForm {
     private Long id;
     private String title;
     private String content;
 
-//    public ArticleForm() {
-//        System.out.println("생성자 호출!!");
-//    }
+    public ArticleForm() {
+        System.out.println("생성자 호출!!");
+    }
+
 
     public Article toEntity() {
         return new Article(id, title, content);
